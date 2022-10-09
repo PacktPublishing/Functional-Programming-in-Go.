e8d8b7efe843e1326e0813fc2db0acc17c104080
Dylan Meeus <meeusdylan@protonmail.com>
2022-10-09 15:28:22 +0000

    bad hotdogshop

---
+++ Chapter4/Examples/HotdogShop/ImpureHotdogShop/pkg/hotdogshop.go
@@ -0,0 +1,24 @@
+package pkg
+
+const (
+	HOTDOG_PRICE = 4
+)
+
+type CreditCard struct {
+	credit int
+}
+
+type Hotdog struct{}
+
+func (c *CreditCard) charge(amount int) {
+	if amount < c.credit {
+		c.credit -= amount
+	} else {
+		panic("no more credit")
+	}
+}
+
+func orderHotdog(c CreditCard) Hotdog {
+	c.charge(HOTDOG_PRICE)
+	return Hotdog{}
+}
